18e910fd3e6c6cb53081352ef7b20a25b78b2197
leijun <ljdzxx@gmail.com>
2026-04-17 03:34:59 +0800

    update rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -8,9 +8,9 @@ ps -ef | grep new-api | grep -v grep | awk 'NR==1{print $2}' | xargs -r kill
 git pull
 
 sleep 3
-cd web
-bun run build
-cd ..
+#cd web
+#bun run build
+#cd ..
 rm -rf ./new-api
 go build -tags noweb -o new-api
 echo 'build finished.'
